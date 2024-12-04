2f2d75e47e9539be101f21066be46d1f7d998486
Lucas Rimondi <lucasrimondi@gmail.com>
2024-12-04 15:47:18 -0300

    fixed github logo size

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -27,7 +27,7 @@ export function Footer() {
               alt="GitHub"
               width={32}
               height={32}
-              className="h-8 w-8 transform transition-transform duration-300 ease-in-out hover:scale-110 sm:h-12 sm:w-12"
+              className="h-8 w-8 transform transition-transform duration-300 ease-in-out hover:scale-110"
             />
           </Link>
         </div>
